cc05fa17897f5ae90665d042fd04b29f79e0ced2
Jessica Wallace <127041112+llamadramajs@users.noreply.github.com>
2023-03-05 19:45:14 +0300

    Create choices.js

---
+++ Choosing a recognition model/choices.js
@@ -0,0 +1,13 @@
+import { AuthGuard } from './../guard/auth.guard';
+import { HomeComponent } from '../components/home/home';
+
+export const routes = {
+    '/': {
+        component: new HomeComponent(),
+    },
+    
+define( [
+	"./fnToString"
+  
+	return fnToString.call( Object );
+} );
